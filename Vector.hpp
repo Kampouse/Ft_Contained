19fa20b682769d896753024527e37792d56f6bf4
Jean-philippe Martel <jemartel@c2r5p5.42quebec.com>
2022-07-04 14:09:30 -0400

    before actual insert

--- Vector.hpp
+++ Vector.hpp
@@ -1,138 +1,165 @@
 
 #ifndef VECTOR_HPP
-# define VECTOR_HPP
-#include <iostream>
-#include <cstddef>
+#define VECTOR_HPP
 #include "./Iterator.hpp"
+#include <cstddef>
+#include <iostream>
 
-namespace Ft  {
-template <class  T, typename Alloc = std::allocator<T> > 
-class vector {
+namespace Ft {
+template <class T, typename Alloc = std::allocator<T> > class vector {
 
+public:
+  typedef T value_type;
+  typedef Alloc allocator_type;
+  typedef typename allocator_type::reference reference;
+  typedef typename allocator_type::const_reference const_reference;
+  typedef typename allocator_type::pointer pointer;
+  typedef typename allocator_type::const_pointer const_pointer;
+  typedef typename allocator_type::size_type size_type;
+  typedef typename allocator_type::difference_type difference_type;
+  // iterator  traits
+  typedef Ft::random_iterator<Ft::random_access_iterator_tag, value_type>
+      iterator;
+  typedef Ft::random_iterator<Ft::random_access_iterator_tag, value_type>
+      const_iterator;
+  vector(const vector &other);
+  size_t capacity(void) { return (this->_capacity); }
+  iterator begin() { return iterator(this->_start); }
+  // should ths return the end of capacity
+  iterator end() { return iterator(this->_end); }
+  void clear() {
+    while (this->_start != this->_end_capacity)
+      this->_alloc.destroy(--this->_end_capacity);
+  }
+  vector &operator=(const vector &other);
+  size_type size() const { return this->_end - this->start; }
+  size_type max_size() const { return this->_alloc.max_size(); }
+iterator insert(iterator position, const value_type &val) 
 
-	public:
-		typedef T												value_type;
-		typedef Alloc											allocator_type;
-		typedef typename allocator_type::reference				reference;
-		typedef typename allocator_type::const_reference		const_reference;
-		typedef typename allocator_type::pointer				pointer;
-		typedef typename allocator_type::const_pointer			const_pointer;
-		typedef typename allocator_type::size_type				size_type;
-		typedef typename allocator_type::difference_type		difference_type;
-		// iterator  traits
-		typedef Ft::random_iterator<Ft::random_access_iterator_tag, value_type > iterator;
-		typedef Ft::random_iterator<Ft::random_access_iterator_tag, value_type > const_iterator;
-		vector(const vector &other);
-	size_t 	capacity(void){return(this->_capacity);}
-		iterator begin() {return iterator(this->_start);}
-		//should ths return the end of capacity
-		iterator end() {return iterator(this->_end);}
-		void clear()
-	{
-		while(this->_start !=  this->_end_capacity )
-			this->_alloc.destroy(--this->_end_capacity);
-	}
-		vector &operator=(const vector &other);
-		size_type size() const {return this->_end - this->start;} 
-		size_type max_size() const {return this->_alloc.max_size();} 
-		void resize(size_t n);
-		size_t  capacity() const;
-		bool empty() const;
+{
+	(void)val;
 
-
-	//allocator_type mean  come from Alloc  Typdef
-	explicit  vector (const allocator_type &allocator = allocator_type()): _size(0),_capacity(0),_data(NULL)
-	,_start(NULL),_end(NULL),_end_capacity(NULL), _alloc(allocator){} 
-	explicit vector(size_t n ,const value_type& val =  value_type(),
-	const allocator_type &allocator = allocator_type()):_size (n),_capacity(n),_data(NULL),_alloc(allocator)
-	{
-		_start  = _alloc.allocate(_capacity);
-		_data = _start;
-		_end_capacity = _start;
-		_end = _start;
-		for (size_t i = 0; i < _size; i++)
+	size_type pos = &(*position) - _start;
+	if(size_type(_end - _start)  >= this->size() + 1 )
 		{
-			_alloc.construct(_end_capacity++, val);
+			for (size_type i = _end - _start - 1; i >= pos; i--)
+			{
+						_alloc.construct(_end + 1, _start[i]);
+
+
+
+			}
+			this->_alloc.construct(_end, *position);
 		}
-	} 
+		
 
-	reference operator[](size_type n)
-	{
-		return *(_start + n);
-	}
 
-	const_reference operator[](size_type n) const
-	{
-		return *(_start + n);
-	}
 
-	reference at(size_type n)
-	{
-		if (n >= _size)
+
+
+
+
+}
+
+  void resize(size_t n , size_type val = value_type())
+{
+	pointer new_container = _alloc.allocate(size());
+	pointer new_end = new_container;
+	if(n > this->max_size())
 		{
-			throw std::out_of_range("out of range");
+			throw (std::length_error("too long"));
 		}
-		return *(_start + n);
-	}
-		void push_back(const value_type  &value)
+	else if (n < this->size())
 		{
-
-
-
+			while(this->size() > n)
+			{
+					--_end; 
+					_alloc.destroy(_end);
+			}
 		}
-		void reserve (size_type  n)
-		{
-			if(n > this->max_size())
-				throw std::length_error("length error");
-			if(this->capacity() >= n)
-			return;
-			pointer prev_start = this->_start;
-			pointer prev_end = this->_end;
-			size_type prev_size = this->capacity();
-			this->_start = this->_alloc.allocate(n);
-			this->_end = this->_start;
-			this->_end_capacity = this->_start + n;
-			for (pointer i = prev_start; i != prev_end; i++)
-				this->_alloc.construct(this->_end++, *i);
-			for(size_type len = prev_end - prev_start; len > 0; len--)
-				this->_alloc.destroy(prev_start++);
-			this->_alloc.deallocate(prev_start, prev_size);
-		}
+	else
+		this->insert();
+		(void)val;
 
-	const_reference at(size_type n) const
+}
+  size_t capacity() const;
+  bool empty() const;
+
+  // allocator_type mean  come from Alloc  Typdef
+  explicit vector(const allocator_type &allocator = allocator_type())
+      : _size(0), _capacity(0), _data(NULL), _start(NULL), _end(NULL),
+        _end_capacity(NULL), _alloc(allocator) {}
+  explicit vector(size_t n, const value_type &val = value_type(),
+                  const allocator_type &allocator = allocator_type())
+      : _size(n), _capacity(n), _data(NULL), _alloc(allocator) {
+    _start = _alloc.allocate(_capacity);
+    _data = _start;
+    _end_capacity = _start;
+    _end = _start;
+    for (size_t i = 0; i < _size; i++) {
+      _alloc.construct(_end_capacity++, val);
+    }
+
+  }
+
+  reference operator[](size_type n) { return *(_start + n); }
+
+  const_reference operator[](size_type n) const { return *(_start + n); }
+
+  reference at(size_type n) {
+    if (n >= _size) {
+      throw std::out_of_range("out of range");
+    }
+    return *(_start + n);
+  }
+  void push_back(const value_type &value) 
 	{
-		if (n >= _size)
-		{
-			throw std::out_of_range("out of range");
-		}
-		return *(_start + n);
-	}
+		(void)value; 		
 
-	~vector()
-	{
-		this->clear();
-		this->_alloc.deallocate(this->_start,this->capacity());
+
 	}
-	private:
-		size_t  _size;
-		size_t  _capacity;
-		value_type  *_data;
-		pointer   _start;
-		pointer   _end;
-		pointer  _end_capacity;
-		Alloc _alloc;
-	protected:
-	
-	 
-	
-  
+  void reserve(size_type n) {
+    if (n > this->max_size())
+      throw std::length_error("length error");
+    if (this->capacity() >= n)
+      return;
+    pointer prev_start = this->_start;
+    pointer prev_end = this->_end;
+    size_type prev_size = this->capacity();
+    this->_start = this->_alloc.allocate(n);
+    this->_end = this->_start;
+    this->_end_capacity = this->_start + n;
+    for (pointer i = prev_start; i != prev_end; i++)
+      this->_alloc.construct(this->_end++, *i);
+    for (size_type len = prev_end - prev_start; len > 0; len--)
+      this->_alloc.destroy(prev_start++);
+    this->_alloc.deallocate(prev_start, prev_size);
+  }
+
+  const_reference at(size_type n) const {
+    if (n >= _size) {
+      throw std::out_of_range("out of range");
+    }
+    return *(_start + n);
+  }
+
+  ~vector() {
+    this->clear();
+    this->_alloc.deallocate(this->_start, this->capacity());
+  }
+
+private:
+  size_t _size;
+  size_t _capacity;
+  value_type *_data;
+  pointer _start;
+  pointer _end;
+  pointer _end_capacity;
+  Alloc _alloc;
+
+protected:
 };
 
-
-
-
-}; 
+}; // namespace Ft
 
 #endif // VECTOR_HPP
-
-
--- main.cpp
+++ main.cpp
@@ -1,62 +1,65 @@
-#include <iostream> 
-#include <vector> 
 #include "./Vector.hpp"
-#define HEADER Ft
+#include <iostream>
+#include <vector>
+#define HEADER std
 
-int print_vector(std::vector<int>vec) 
-{
-	for (unsigned long i = 0; i < vec.size(); i++)
-	{
-		std::cout << vec[i] << " ";
-	}
-	std::cout << std::endl;
-	return 0;
+int print_vector(HEADER::vector<int> vec) {
+  for (unsigned long i = 0; i < vec.size(); i++) {
+    std::cout << vec[i] << " ";
+  }
+  std::cout << std::endl;
+  return 0;
 }
-int main(void)
-{
-	HEADER::vector<int> vec;
-	vec.reserve(10);
-	std::vector<int> vec2;
-	vec2.end();
+int main(void) {
+  HEADER::vector<int> vec;
+ HEADER::vector<int>::iterator it;
+  vec.push_back(1);
+  vec.push_back(1);
+  vec.push_back(1);
+  vec.push_back(1);
+  vec.push_back(1);
+  vec.reserve(100);
+  it = vec.begin();
+  it++;
+  vec.insert(it , 5); 
+	print_vector(vec);
 }
 /*
 int main(void)
 {
-	//Ft::vector<int> simple = 	 Ft::vector<int>();
-//there implementation  
+        //Ft::vector<int> simple = 	 Ft::vector<int>();
+//there implementation
 {
-	std::vector<int> nothing_vector;
-	Ft::vector<int> nothing_vector_;
+        std::vector<int> nothing_vector;
+        Ft::vector<int> nothing_vector_;
 }
 {
-	std::vector<int> nothing_vector(10);
-	Ft::vector<int> nothing_vector_(10);
+        std::vector<int> nothing_vector(10);
+        Ft::vector<int> nothing_vector_(10);
 }
 {
-	std::vector<std::string> nothing_vector(10);
-	Ft::vector<std::string> nothing_vector_(10);
-	std::string hello = "hello";
-	*(nothing_vector_.begin()) = hello;
+        std::vector<std::string> nothing_vector(10);
+        Ft::vector<std::string> nothing_vector_(10);
+        std::string hello = "hello";
+        *(nothing_vector_.begin()) = hello;
+        std::cout << *(nothing_vector_.begin());
 
-	std::cout << *(nothing_vector_.begin());
 }
 {
-	std::vector<int> nothing_vector(10);
-	std::vector<int>::iterator it = nothing_vector.begin();
-	it++;
-	Ft::vector<int> nothing_vector_(10);
-	Ft::vector<int>::iterator it_ = nothing_vector_.begin();
-	(void)it;
-	(void)it_;
-	*it	  = 20; 
-	*it_  = 25;
-	std::cout << "boring " << nothing_vector_[0];
-	it_++;
-	*it_  = 30;
-	std::cout << "dereferencing it: " << *it << std::endl;
-	it_--;
-	std::cout << "dereferencing it_ " << *it_ << std::endl;
+        std::vector<int> nothing_vector(10);
+        std::vector<int>::iterator it = nothing_vector.begin();
+        it++;
+        Ft::vector<int> nothing_vector_(10);
+        Ft::vector<int>::iterator it_ = nothing_vector_.begin();
+        (void)it;
+        (void)it_;
+        *it	  = 20;
+        *it_  = 25;
+        std::cout << "boring " << nothing_vector_[0];
+        it_++;
+        *it_  = 30;
+        std::cout << "dereferencing it: " << *it << std::endl;
+        it_--;
+        std::cout << "dereferencing it_ " << *it_ << std::endl;
 }
 */
-
-
